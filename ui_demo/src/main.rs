bfdbb4e0ddd13e1c763c40186f62a7f89f958cc5
silloi <silloi1993@gmail.com>
2022-02-08 18:28:55 +0900

    create ui_demo

---
+++ ui_demo/src/main.rs
@@ -0,0 +1,33 @@
+use iced::{executor, Application, Clipboard, Command, Element, Settings, Text};
+
+struct GUI;
+
+impl Application for GUI {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
+        (GUI, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("DEMO")
+    }
+
+    fn update(
+        &mut self,
+        _message: Self::Message,
+        _clipboard: &mut Clipboard,
+    ) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, World!").into()
+    }
+}
+
+fn main() -> iced::Result {
+    GUI::run(Settings::default())
+}
