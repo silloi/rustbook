7d8f1359d49bbe050ed3b2a92147b726f77283bc
silloi <silloi1993@gmail.com>
2022-02-08 20:12:59 +0900

    implement messages

--- ui_demo/src/main.rs
+++ ui_demo/src/main.rs
@@ -3,19 +3,33 @@ use iced::{
     HorizontalAlignment, Length, Row, Settings, Text,
 };
 
+pub enum TickState {
+    Stopped,
+    Ticking,
+}
+
 struct GUI {
+    tick_state: TickState,
     start_stop_button_state: button::State,
     reset_button_state: button::State,
 }
 
+#[derive(Debug, Clone)]
+pub enum Message {
+    Start, // 時間の測定を開始するメッセージ
+    Stop,  // 時間の測定を停止するメッセージ
+    Reset, // 時間の測定をリセットするメッセージ
+}
+
 impl Application for GUI {
     type Executor = executor::Default;
-    type Message = ();
+    type Message = Message;
     type Flags = ();
 
     fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
         (
             GUI {
+                tick_state: TickState::Stopped,
                 start_stop_button_state: button::State::new(),
                 reset_button_state: button::State::new(),
             },
@@ -29,29 +43,54 @@ impl Application for GUI {
 
     fn update(
         &mut self,
-        _message: Self::Message,
+        message: Self::Message,
         _clipboard: &mut Clipboard,
     ) -> Command<Self::Message> {
+        match message {
+            Message::Start => {
+                self.tick_state = TickState::Ticking;
+            }
+            Message::Stop => {
+                self.tick_state = TickState::Stopped;
+            }
+            Message::Reset => {}
+        }
         Command::none()
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        // init widgets
-        let tick_text = Text::new("00:00:00:00").font(FONT).size(60);
-        let start_stop_button = Button::new(
-            &mut self.start_stop_button_state,
-            Text::new("Start")
+        // prepare duration text
+        let duration_text = "00:00:00:00";
+
+        // prepare start/stop text
+        let start_stop_text = match self.tick_state {
+            TickState::Stopped => Text::new("Start")
                 .horizontal_alignment(HorizontalAlignment::Center)
                 .font(FONT),
-        )
-        .min_width(80);
+            TickState::Ticking => Text::new("Stop")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        };
+
+        // prepare start/stop message on button press
+        let start_stop_message = match self.tick_state {
+            TickState::Stopped => Message::Start,
+            TickState::Ticking => Message::Stop,
+        };
+
+        // init widgets
+        let tick_text = Text::new(duration_text).font(FONT).size(60);
+        let start_stop_button = Button::new(&mut self.start_stop_button_state, start_stop_text)
+            .min_width(80)
+            .on_press(start_stop_message);
         let reset_button = Button::new(
             &mut self.reset_button_state,
             Text::new("Reset")
                 .horizontal_alignment(HorizontalAlignment::Center)
                 .font(FONT),
         )
-        .min_width(80);
+        .min_width(80)
+        .on_press(Message::Reset);
 
         // prepare column
         Column::new()
