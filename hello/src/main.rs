ae70ef4e74ea00be85db2ca2c44cfc64389ccc11
silloi <silloi1993@gmail.com>
2022-02-09 22:28:18 +0900

    implement embedded uart and i2c

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,8 +4,13 @@
 extern crate panic_halt;
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::debug;
 use stm32f4xx_hal::{delay::Delay, prelude::*, stm32};
+
+use embedded_hal::serial::Write;
+use stm32f4xx_hal::serial::{config::Config, config::StopBits, Serial};
+
+use stm32f4xx_hal::i2c::I2c;
 
 #[entry]
 fn main() -> ! {
@@ -25,6 +30,36 @@ fn main() -> ! {
             led.set_low().unwrap();
             delay.delay_ms(100_u32);
         }
+
+        let gpioc = dp.GPIOC.split();
+        let config = Config::default()
+            .baudrate(115200.bps())
+            .wordlength_8()
+            .parity_none()
+            .stopbits(StopBits::STOP1);
+
+        let pins = (
+            gpioc.pc10.into_alternate_af8(),
+            gpioc.pc11.into_alternate_af8(),
+        );
+        let mut uart4 = Serial::uart4(dp.UART4, pins, config, clocks).unwrap();
+
+        let _ = uart4.write(b'H');
+        let _ = uart4.write(b'e');
+        let _ = uart4.write(b'l');
+        let _ = uart4.write(b'l');
+        let _ = uart4.write(b'o');
+        let _ = uart4.flush();
+
+        let gpiob = dp.GPIOB.split();
+
+        let pins = (
+            gpiob.pb8.into_alternate_af4().set_open_drain(),
+            gpiob.pb9.into_alternate_af4().set_open_drain(),
+        );
+        let mut i2c = I2c::new(dp.I2C1, pins, 400.khz(), clocks);
+
+        let _ = i2c.write(0, &[0]);
     }
 
     debug::exit(debug::EXIT_SUCCESS);
