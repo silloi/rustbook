0acfe44765232d24bee02aa98f81ec92f4cd8dba
silloi <silloi1993@gmail.com>
2022-02-10 22:06:18 +0900

    create benchmark and profiler samples

---
+++ sample_bench/benches/hash.rs
@@ -0,0 +1,15 @@
+#![feature(test)]
+
+extern crate test;
+
+use sample_bench::hash;
+use test::Bencher;
+
+#[bench]
+fn bench_hash(b: &mut Bencher) {
+    b.iter(|| {
+        let n = test::black_box(2);
+        hash(n)
+    });
+}
+
